d8168be18cd0e04f05e39c2f0fdffb5333078f51
Dawodu Bolaji <dawodubolaji189@gmail.com>
2024-05-11 20:47:54 +0100

    finalize the app

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -8,7 +8,7 @@ const AnimatedCounter = ({ amount }: { amount: number} ) => {
         <CountUp 
           end={amount}
           decimal=","
-          prefix="₦"
+          prefix="$"
           decimals={2}/>
     </div>
   )
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,6 @@
 import { logoutAccount } from '@/lib/actions/user.actions';
 import Image from 'next/image'
 import { useRouter } from 'next/navigation';
-import React from 'react'
 
 const Footer = ({ type = 'desktop', user }: FooterProps) => {
     const router = useRouter();
@@ -16,13 +15,13 @@ const Footer = ({ type = 'desktop', user }: FooterProps) => {
     <footer className="footer">
         <div className={type === 'mobile' ? 'footer_name-mobile' : 'footer_name'}>
             <p className="text-xl font-bold text-gray-700">
-                {user?.name[0]}
+                {user?.firstName[0]}
             </p>
         </div>
 
         <div className={type === 'mobile' ? 'footer_email-mobile' : 'footer_email'}>
           <h1 className="text-14 truncate font-semibold text-gray-700">
-            {user?.name}
+            {user?.firstName}
           </h1>
           <p className="text-14 truncate font-normal text-gray-600">
             {user?.email}
